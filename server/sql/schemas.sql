32c0ab0983bdb2fd1288ad0d806dae194ed5c7dc
Jeff Squyres <jsquyres@cisco.com>
2006-09-01 15:57:14 +0000

    Arrgh.  This is the schema for the current live...
    Arrgh.  This is the schema for the current live mtt database.  It was
    accidentally committed to the /tmp/bulk-submit tree earlier today.
    
    This commit was SVN r277.

--- server/sql/schemas.sql
+++ server/sql/schemas.sql
@@ -4,6 +4,13 @@
 -- Usage: $ cat schemas.sql | psql -d <dbname> -U <dbusername>
 --
 
+DROP TABLE once;
+DROP TABLE runs;
+DROP TABLE installs;
+DROP TABLE builds;
+DROP TABLE general_a;
+DROP TABLE general_b;
+
 CREATE TABLE once (
     run_index integer DEFAULT 1,
     mtt_version_major integer,
@@ -16,25 +23,23 @@ CREATE TABLE once (
     hostname character varying(256) DEFAULT ''::character varying,
     mpi_name character varying(256) DEFAULT ''::character varying,
     mpi_version character varying(256) DEFAULT ''::character varying,
-    submitting_unix_user character varying(256) DEFAULT ''::character varying,
-    submitting_http_user character varying(256) DEFAULT ''::character varying,
     unique (run_index,hostname,os_name,os_version,platform_hardware,platform_type,mpi_name,mpi_version)
 );
 
 -- All three phases require these fields
 CREATE TABLE general_a (
     run_index integer,
-    test_result smallint,
-    timed_out boolean,
+    success boolean,
     mpi_get_section_name character varying(256) DEFAULT 'N/A'::character varying,
     mpi_install_section_name character varying(256) DEFAULT 'N/A'::character varying,
     mpi_details character varying(256) DEFAULT 'N/A'::character varying,
+    configure_arguments character varying(256) DEFAULT 'N/A'::character varying,
     merge_stdout_stderr integer DEFAULT 0,
     environment text DEFAULT 'N/A'::text,
     vpath_mode character varying(256) DEFAULT 'N/A'::character varying,
     result_message character varying(256),
     stderr text DEFAULT 'N/A'::text,
-    stdout text DEFAULT 'N/A'::text,
+    "stdout" text DEFAULT 'N/A'::text,
     start_run_timestamp timestamp without time zone,
     start_test_timestamp timestamp without time zone,
     submit_test_timestamp timestamp without time zone,
@@ -44,24 +49,24 @@ CREATE TABLE general_a (
 -- MPI Install and Test Build phases require these fields
 CREATE TABLE general_b (
     compiler_name character varying(256) DEFAULT 'N/A'::character varying,
-    compiler_version character varying(256) DEFAULT 'N/A'::character varying
+    compiler_version character varying(256) DEFAULT 'N/A'::character varying,
+    configure_arguments character varying(256) DEFAULT 'N/A'::character varying
 );
 
 CREATE TABLE installs (
-    configure_arguments character varying(256) DEFAULT 'N/A'::character varying
 )
-INHERITS (general_a,general_b);
+INHERITS (general_a, general_b);
 
 CREATE TABLE builds (
     test_build_section_name character varying(256) DEFAULT 'N/A'::character varying
-) 
-INHERITS (general_a,general_b);
+)
+INHERITS (general_a, general_b);
 
 CREATE TABLE runs (
-    test_build_section_name character varying(256) DEFAULT 'N/A'::character varying,
     test_command character varying(256) DEFAULT 'N/A'::character varying,
     test_name character varying(256),
     test_np integer,
+    test_build_section_name character varying(256) DEFAULT 'N/A'::character varying,
     test_run_section_name character varying(256) DEFAULT 'N/A'::character varying
 )
 INHERITS (general_a);
