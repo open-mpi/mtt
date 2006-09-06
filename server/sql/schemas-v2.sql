04b9717e405790c2e02e27eb21b291ffce9ba62a
Ethan Mallove <Ethan.Mallove@oracle.com>
2006-09-06 13:46:00 +0000

    refs #55
     * Any column that will be used in a {{{JOIN}}} statement, deserves an
       index. (To avoid symbol name collisions, the table name is prepended
       to the index name.)
     * {{{tstamp}}} deserves an index because it is part of the {{{WHERE}}}
       clause in all queries.  
     * Fixed SQL syntax errors
    
    This commit was SVN r312.

--- server/sql/schemas-v2.sql
+++ server/sql/schemas-v2.sql
@@ -10,7 +10,7 @@ CREATE TABLE cluster (
        platform_id character varying(256) NOT NULL,
        platform_hardware character varying(256) NOT NULL,
        platform_type character varying(256) NOT NULL,
-       os_name character varying(256) NOT NUL,
+       os_name character varying(256) NOT NULL,
        os_version character varying(256) NOT NULL,
        UNIQUE (os_name,os_version,platform_hardware,platform_type,platform_id)
 );
@@ -19,7 +19,7 @@ DROP TABLE users;
 CREATE TABLE users (
        users_id serial,
        address character(64) NOT NULL,
-       gecos character(32) NOT NULL,
+       gecos character(32) NOT NULL
 );
 
 DROP TABLE cluster_owner;
@@ -28,6 +28,11 @@ CREATE TABLE cluster_owner (
        cluster_id integer, --> refers to cluster table
        users_id integer --> refers to users table
 );
+
+DROP INDEX cluster_owner_users_idx;
+CREATE INDEX cluster_owner_users_idx ON cluster_owner(users_id);
+DROP INDEX cluster_owner_cluster_idx;
+CREATE INDEX cluster_owner_cluster_idx ON cluster_owner(cluster_id);
 
 -- Serial number used for individual MTT runs
 DROP SEQUENCE client_serial;
@@ -42,13 +47,20 @@ CREATE TABLE submit (
 	hostname character varying(128) NOT NULL,
 	local_username character varying(16) NOT NULL,
 	http_username character varying(16) NOT NULL,
-	tstamp timestamp without timezone,
+	tstamp timestamp without time zone,
 	-- phase value: 1=mpi_install, 2=test_build, 3=test_run
 	phase smallint,
 	-- phase_id will be an index into mpi_install, test_build, or
         -- test_run tables, depending on value of phase
 	phase_id integer
 );
+
+DROP INDEX submit_serial_idx;
+CREATE INDEX submit_serial_idx ON submit(serial_id);
+DROP INDEX submit_tstamp_idx;
+CREATE INDEX submit_tstamp_idx ON submit(tstamp);
+DROP INDEX submit_phase_idx;
+CREATE INDEX submit_phase_idx ON submit(phase_id);
 
 DROP TABLE mpi_get;
 CREATE TABLE mpi_get (
@@ -77,6 +89,15 @@ CREATE TABLE mpi_install (
 	results_id integer --> refers to results table, this changes every night
 );
 
+DROP INDEX mpi_install_cluster_idx;
+CREATE INDEX mpi_install_cluster_idx ON mpi_install(cluster_id);
+DROP INDEX mpi_install_mpi_get_idx;
+CREATE INDEX mpi_install_mpi_get_idx ON mpi_install(mpi_get_id);
+DROP INDEX mpi_install_compiler_idx;
+CREATE INDEX mpi_install_compiler_idx ON mpi_install(compiler_id);
+DROP INDEX mpi_install_results_idx;
+CREATE INDEX mpi_install_results_idx ON mpi_install(results_id);
+
 DROP TABLE test_build;
 CREATE TABLE test_build (
 	test_build_id serial, --> this changes every night
@@ -85,21 +106,33 @@ CREATE TABLE test_build (
 	suite_name character varying(64) NOT NULL,  --> *** do not know how to standardize this
 	compiler_id integer, --> refers to compiler table
 
-	results_id integer, --> refers to results table, this changes every night
+	results_id integer --> refers to results table, this changes every night
 );
+
+DROP INDEX test_build_mpi_install_idx;
+CREATE INDEX test_build_mpi_install_idx ON test_build(mpi_install_id);
+DROP INDEX test_build_compiler_idx;
+CREATE INDEX test_build_compiler_idx ON test_build(compiler_id);
+DROP INDEX test_build_results_idx;
+CREATE INDEX test_build_results_idx ON test_build(results_id);
 
 DROP TABLE test_run;
 CREATE TABLE test_run (
 	test_run_id serial,
-	test_build_id --> refers to test_build table
+	test_build_id integer,--> refers to test_build table
 
 	variant smallint,
 	name character varying(64) NOT NULL,
 	command text NOT NULL,
 	np smallint,
 
-	results_id integer, --> refers to results table
+	results_id integer --> refers to results table
 );
+
+DROP INDEX test_build_idx;
+CREATE INDEX test_build_idx ON test_run(test_build_id);
+DROP INDEX results_idx;
+CREATE INDEX results_idx ON test_run(results_id);
 
 DROP TABLE results;
 CREATE TABLE results (
@@ -109,10 +142,13 @@ CREATE TABLE results (
 	merge_stdout_stderr boolean,
 	result_stdout text, --> what is the largest text blob we can put in PG?  Rich says default might be 8k!
 	result_stderr text,
-	start_timestamp timestamp without timezone,
-	stop_timestamp timestamp without timezone,
+	start_timestamp timestamp without time zone,
+	stop_timestamp timestamp without time zone,
 	-- do we want exit status?
-	exit_status smallint
+	exit_status smallint,
 	-- result value: 1=pass, 2=fail, 3=skipped, 4=timed out
-	result smallint,
-};
+	result smallint
+);
+
+DROP INDEX results_result_idx;
+CREATE INDEX results_result_idx ON results(result);
