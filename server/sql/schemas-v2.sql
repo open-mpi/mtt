02a8a772dc1f2321375eda2526f62ee606c91b25
Ethan Mallove <Ethan.Mallove@oracle.com>
2006-09-20 16:11:26 +0000

    * Use defaults, in case a parameter is not fil...
     * Use defaults, in case a parameter is not filled in by the client.
     * vpath_mode is currently coming over from the client as a string, but
       should eventually be changed to a bool
    
    ([http://www.eggheadcafe.com/articles/20050826.asp link to below tip])
    
    == A table should avoid nullable columns ==
    
    Tables can have columns defined to allow null values. A null value
    indicates that there is no value. Although it can be useful to allow
    null values in isolated cases, it is best to use them sparingly because
    they require special handling that increases the complexity of data
    operations. For best performance, and to reduce bugs, columns should
    ideally be set to NOT NULL. For example, use of the IS NULL keywords in
    the WHERE clause makes that portion of the query unable to use an index.
    One simple way to avoid nullable columns is to set them as NOT NULL and
    provide a default value when creating the column.
    
    This commit was SVN r343.

--- server/sql/schemas-v2.sql
+++ server/sql/schemas-v2.sql
@@ -7,32 +7,13 @@
 DROP TABLE compute_cluster;
 CREATE TABLE compute_cluster (
        compute_cluster_id serial,
-       platform_id character varying(256) NOT NULL,
-       platform_hardware character varying(256) NOT NULL,
-       platform_type character varying(256) NOT NULL,
-       os_name character varying(256) NOT NULL,
-       os_version character varying(256) NOT NULL,
+       platform_id character varying(256) NOT NULL DEFAULT '',
+       platform_hardware character varying(256) NOT NULL DEFAULT '',
+       platform_type character varying(256) NOT NULL DEFAULT '',
+       os_name character varying(256) NOT NULL DEFAULT '',
+       os_version character varying(256) NOT NULL DEFAULT '',
        UNIQUE (os_name,os_version,platform_hardware,platform_type,platform_id)
 );
-
-DROP TABLE users;
-CREATE TABLE users (
-       users_id serial,
-       address character(64) NOT NULL,
-       gecos character(32) NOT NULL
-);
-
-DROP TABLE cluster_owner;
-CREATE TABLE cluster_owner (
-       cluster_owner_id serial,
-       compute_cluster_id integer, --> refers to compute_cluster table
-       users_id integer --> refers to users table
-);
-
-DROP INDEX cluster_owner_users_idx;
-CREATE INDEX cluster_owner_users_idx ON cluster_owner(users_id);
-DROP INDEX cluster_owner_cluster_idx;
-CREATE INDEX cluster_owner_cluster_idx ON cluster_owner(compute_cluster_id);
 
 -- Serial number used for individual MTT runs
 DROP SEQUENCE client_serial;
@@ -41,13 +22,13 @@ CREATE SEQUENCE client_serial;
 DROP TABLE submit;
 CREATE TABLE submit (
 	submit_id serial,
-	serial_id integer, --> refers to the serial sequence
-	mtt_version_major smallint,
-	mtt_version_minor smallint,
-	hostname character varying(128) NOT NULL,
-	local_username character varying(16) NOT NULL,
-	http_username character varying(16) NOT NULL,
-	tstamp timestamp without time zone
+	serial_id integer NOT NULL DEFAULT -1, --> refers to the serial sequence
+	mtt_version_major smallint NOT NULL DEFAULT -1,
+	mtt_version_minor smallint NOT NULL DEFAULT -1,
+	hostname character varying(128) NOT NULL DEFAULT '',
+	local_username character varying(16) NOT NULL DEFAULT '',
+	http_username character varying(16) NOT NULL DEFAULT '',
+	tstamp timestamp without time zone NOT NULL DEFAULT now()
 );
 
 DROP INDEX submit_serial_idx;
@@ -60,28 +41,28 @@ CREATE INDEX submit_phase_idx ON submit(phase_id);
 DROP TABLE mpi_get;
 CREATE TABLE mpi_get (
 	mpi_get_id serial,
-	section_name character varying(64) NOT NULL,
-	version character varying(32) NOT NULL
+	section_name character varying(64) NOT NULL DEFAULT '',
+	version character varying(32) NOT NULL DEFAULT ''
 );
 
 DROP TABLE compiler;
 CREATE TABLE compiler (
 	compiler_id serial,
-	compiler_name character varying(64) NOT NULL,
-	compiler_version character varying(64) NOT NULL
+	compiler_name character varying(64) NOT NULL DEFAULT '',
+	compiler_version character varying(64) NOT NULL DEFAULT ''
 );
 
 DROP TABLE mpi_install;
 CREATE TABLE mpi_install (
 	mpi_install_id serial,
 
-	compute_cluster_id integer, --> refers to compute_cluster table
-	mpi_get_id integer, --> refers to mpi_get table
-	compiler_id integer, --> refers to compiler table
-	configure_arguments character varying(512), --> put this into separate table because substring searchs will be much faster, but rich says that this is a fairly uncommon way to search for our results, so the PITA for putting this in another table might not be worth it
-	vpath_mode smallint,
+	compute_cluster_id integer NOT NULL DEFAULT -1, --> refers to compute_cluster table
+	mpi_get_id integer NOT NULL DEFAULT -1, --> refers to mpi_get table
+	compiler_id integer NOT NULL DEFAULT -1, --> refers to compiler table
+	configure_arguments character varying(512) NOT NULL DEFAULT '', --> put this into separate table because substring searchs will be much faster, but rich says that this is a fairly uncommon way to search for our results, so the PITA for putting this in another table might not be worth it
+	vpath_mode character varying(16) NOT NULL DEFAULT '',
 
-	results_id integer --> refers to results table, this changes every night
+	results_id integer NOT NULL DEFAULT -1 --> refers to results table, this changes every night
 );
 
 DROP INDEX mpi_install_compute_cluster_idx;
@@ -96,12 +77,12 @@ CREATE INDEX mpi_install_results_idx ON mpi_install(results_id);
 DROP TABLE test_build;
 CREATE TABLE test_build (
 	test_build_id serial, --> this changes every night
-	mpi_install_id integer, --> refers to mpi_install table
+	mpi_install_id integer NOT NULL DEFAULT -1, --> refers to mpi_install table
 
-	suite_name character varying(64) NOT NULL,  --> *** do not know how to standardize this
-	compiler_id integer, --> refers to compiler table
+	suite_name character varying(64) NOT NULL DEFAULT '',  --> *** do not know how to standardize this 
+	compiler_id integer NOT NULL DEFAULT -1, --> refers to compiler table
 
-	results_id integer --> refers to results table, this changes every night
+	results_id integer NOT NULL DEFAULT -1 --> refers to results table, this changes every night
 );
 
 DROP INDEX test_build_mpi_install_idx;
@@ -114,15 +95,15 @@ CREATE INDEX test_build_results_idx ON test_build(results_id);
 DROP TABLE test_run;
 CREATE TABLE test_run (
 	test_run_id serial,
-	test_build_id integer,--> refers to test_build table
+	test_build_id integer NOT NULL DEFAULT -1,--> refers to test_build table
 
-	variant smallint,
-	test_name character varying(64) NOT NULL,
-	command text NOT NULL,
-	np smallint,
+	variant smallint NOT NULL DEFAULT -1,
+	test_name character varying(64) NOT NULL DEFAULT '',
+	command text NOT NULL DEFAULT '',
+	np smallint NOT NULL DEFAULT -1,
 
-	results_id integer, --> refers to results table
-    failure_id integer DEFAULT NULL  --> points to information about failure
+	results_id integer NOT NULL DEFAULT -1, --> refers to results table
+    failure_id integer NOT NULL DEFAULT -1  --> points to information about failure
                                      --> null if it's yet to be "churned"
 );
 
@@ -134,18 +115,18 @@ CREATE INDEX results_idx ON test_run(results_id);
 DROP TABLE results;
 CREATE TABLE results (
 	results_id serial,
-	submit_id integer,
+	submit_id integer NOT NULL DEFAULT -1,
 
-	environment text,
+	environment text NOT NULL DEFAULT '',
 	merge_stdout_stderr boolean,
-	result_stdout text, --> what is the largest text blob we can put in PG?  Rich says default might be 8k!
-	result_stderr text,
+	result_stdout text NOT NULL DEFAULT '', --> what is the largest text blob we can put in PG?  Rich says default might be 8k!
+	result_stderr text NOT NULL DEFAULT '',
 	start_timestamp timestamp without time zone,
 	stop_timestamp timestamp without time zone,
 	-- do we want exit status?
-	exit_status smallint,
+	exit_status smallint NOT NULL DEFAULT -1,
 	-- success  value: 1=pass, 2=fail, 3=skipped, 4=timed out
-	success smallint
+	success smallint NOT NULL DEFAULT -1
 );
 
 DROP INDEX results_success_idx;
@@ -155,9 +136,29 @@ CREATE INDEX results_success_idx ON results(success);
 
 DROP TABLE failure;
 CREATE TABLE failure (
-    failure_id integer,
+    failure_id integer NOT NULL DEFAULT -1,
     timestamp timestamp without time zone,  --> first time the failure occurred
-    field character varying(16) NOT NULL,   --> maps to any non *_id field name in mtt database
-    value character varying(16) NOT NULL    --> value of field
+    field character varying(16) NOT NULL DEFAULT '',   --> maps to any non *_id field name in mtt database
+    value character varying(16) NOT NULL DEFAULT ''   --> value of field 
 );
+
+DROP TABLE users;
+CREATE TABLE users (
+       users_id serial,
+       address character(64) NOT NULL DEFAULT '',
+       gecos character(32) NOT NULL DEFAULT ''
+);
+
+DROP TABLE cluster_owner;
+CREATE TABLE cluster_owner (
+       cluster_owner_id serial,
+       compute_cluster_id integer NOT NULL DEFAULT -1, --> refers to compute_cluster table
+       users_id integer NOT NULL DEFAULT -1 --> refers to users table
+);
+
+DROP INDEX cluster_owner_users_idx;
+CREATE INDEX cluster_owner_users_idx ON cluster_owner(users_id);
+DROP INDEX cluster_owner_cluster_idx;
+CREATE INDEX cluster_owner_cluster_idx ON cluster_owner(compute_cluster_id);
+
 
