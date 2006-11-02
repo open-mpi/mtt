03d6e86912cb4b5ca2eda3197e5e57c2dfbaa1dc
Ethan Mallove <Ethan.Mallove@oracle.com>
2006-11-02 23:44:51 +0000

    Added [source:/trunk/lib/MTT/Test/Analyze/Perfo...
    Added [source:/trunk/lib/MTT/Test/Analyze/Performance/IMB.pm] for Intel
    Performance (aka Pallas) tests.  The plugin requires that only one
    individual benchmark and one {{{np}}} be run at a time (E.g.,
    {{{PMB-MPI1 PingPong -npmin n}}})
    
    refs #13
    
    This commit was SVN r411.

---
+++ lib/MTT/Test/Analyze/Performance/IMB.pm
@@ -0,0 +1,148 @@
+#!/usr/bin/env perl
+#
+# Copyright (c) 2006 Sun Microsystems, Inc.  All rights reserved.
+# $COPYRIGHT$
+#
+# Additional copyrights may follow
+#
+# $HEADER$
+#
+
+package MTT::Test::Analyze::Performance::IMB;
+
+use strict;
+use Data::Dumper;
+
+# Process the stdout emitted from an IMB test
+sub Analyze {
+
+    my($stdout) = @_;
+    my $report;
+    my(@bytes,
+       @times,
+       @headers,
+       $data,
+       @mbps,
+       @usec,
+       $bandwidth_unit,
+       $latency_unit);
+
+    print "\nstdout = " . Dumper($stdout);
+
+    my @lines = split(/\n|\r/, $stdout);
+
+    while (my $line = shift(@lines)) {
+
+        if ($line =~ /benchmarking\s+(\w+)/i) {
+            $report->{test_name} = $1;
+            last;
+        }
+    }
+    my $lat_units = '\b\w*sec(?:onds?)?';
+
+    # Grab the table headers
+    while (my $line = shift(@lines)) {
+
+        # Possible headers:
+        # #bytes #repetitions      t[usec]                             Mbytes/sec
+        # #bytes #repetitions  t_min[usec]  t_max[usec]  t_avg[usec]   Mbytes/sec
+        # #bytes #repetitions  t_min[usec]  t_max[usec]  t_avg[usec]
+        #        #repetitions  t_min[usec]  t_max[usec]  t_avg[usec]
+
+        if ($line =~
+                (/(?:\#?\s*(bytes) \s+)?
+                  (?:\#?\s*(repetitions) \s+)
+                  (?:
+                      (?:\bt_?(   \[?$lat_units\]?) \s*)
+                      |
+                      (?:\bt_?(min\[?$lat_units\]?) \s*)
+                      (?:\bt_?(max\[?$lat_units\]?) \s*)
+                      (?:\bt_?(avg\[?$lat_units\]?) \s*)
+                  )
+                  (?:(\b\w*bytes.sec(?:ond)?)|\w?bps)?
+                 /ix)) {
+
+            my $match;
+            foreach my $i ((1..7)) {
+                eval '$match = $' . $i . ';';
+                push(@headers, $match) if (defined($match));
+            }
+            last;
+        }
+    }
+
+    # Grab the table body
+    my $rows = 0;
+    while (my $line = shift(@lines)) {
+        if ($line =~
+                (/
+                  (?:([\d\.]+) \s*)
+                  (?:([\d\.]+) \s*)
+                  (?:([\d\.]+) \s*)
+                  (?:([\d\.]+) \s*)?
+                  (?:([\d\.]+) \s*)?
+                  (?:([\d\.]+) \s*)?
+                 /ix)) {
+
+            my $i = 1;
+            my $match;
+            my @headers_tmp = @headers;
+
+            foreach my $i ((1..7)) {
+                my $header = shift @headers_tmp;
+
+                eval '$match = $' . $i . ';';
+
+                if ($match) {
+                    push(@{$data->{$header}}, $match);
+                }
+            }
+            $rows++;
+
+        } elsif ($line =~ /^\s*$/) {
+            last;
+        }
+    }
+
+    $report->{test_type} = 'latency_bandwidth';
+
+    foreach my $k (keys %$data) {
+
+        # Careful how header labels are parsed. E.g., 
+        # bandwidth and message_size could contain some
+        #   variation of string "byte"
+        # bandwidth and latency could contain some
+        #   variation of string "second"
+
+        if ($k =~ /(min|max|avg)?\[$lat_units\]/i) {
+            my $agg = (defined($1) ? $1 : "avg");
+            $report->{"latency_$agg"} = 
+                "{" . join(",", map { trim($_) } @{$data->{$k}}) . "}";
+
+        } elsif ($k =~ /[kmbpt]?byte.*sec|[kmbpt]?bps/i) {
+            $report->{"bandwidth_avg"} =
+                "{" . join(",", map { trim($_) } @{$data->{$k}}) . "}";
+
+        } elsif ($k =~ /byte|[kmbpt]b/i) {
+            $report->{message_size} =
+                "{" . join(",", map { trim($_) } @{$data->{$k}}) . "}";
+        }
+    }
+
+    if (! defined($report->{message_size})) {
+        $report->{message_size} = 
+            "{" . join(",", map { "0" } (1..$rows)) . "}";
+    }
+
+    print "\nreport = " . Dumper($report);
+
+    return $report;
+}
+
+# Trim leading/trailing whitespace
+sub trim {
+    s/^\s+|\s+$//;
+    return $_;
+}
+
+1;
