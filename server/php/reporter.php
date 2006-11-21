abf11afdfaf3c26b1308df85295b42e28a0de709
Ethan Mallove <Ethan.Mallove@oracle.com>
2006-11-21 23:09:57 +0000

    Checkpoint before implementing detail reports. ...
    Checkpoint before implementing detail reports. So far ...
    
     * Got summary reports (all three phases) going with new
       schema [1] [2]
     * Pruned out a lot of cruft
     * Slimmed down screen (added popus for multiple selection)
     * Cut back on use of {{{global}}}
    
    [1] [source:/trunk/server/sql/schemas-v2.sql new schema]
    [2] [source:/trunk/server/sql/views.sql views]
    
    This commit was SVN r448.

--- server/php/reporter.php
+++ server/php/reporter.php
@@ -35,51 +35,20 @@ if ($GLOBALS['verbose'])
 else
     error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
-# X: which .inc's do we really need?
 $topdir = ".";
 include_once("/l/osl/www/doc/www.open-mpi.org/dbpassword.inc");
 include_once("$topdir/reporter.inc");
 include_once("$topdir/screen.inc");
 include_once("$topdir/report.inc");
-include_once("$topdir/head.inc");
-include_once("$topdir/http.inc");
-include_once("$topdir/html.inc");
-include_once("$topdir/database.inc");
 
 # In case we're using this script from the command-line
 if ($argv)
     $_GET = getoptions($argv);
 
-$form_id = "report";
+dump_query_screen();
 
-# Print html head (query frame & results frame may need this script/style)
-$html_head = "";
-$html_head .= "\n<html>";
-$html_head .= "\n<head><title>Open MPI Test Reporter</title>";
-
-$html_head .= "\n<script language='javascript' type='text/javascript'>";
-$html_head .= "\n$javascript";
-$html_head .= "\n</script>";
-
-$html_head .= "\n<style type='text/css'>";
-$html_head .= "\n$style";
-$html_head .= "\n</style>";
-$html_head .= "\n</head>";
-
-print $html_head;
-
-$_GET['1-page'] = isset($_GET['1-page']) ? $_GET['1-page'] : 'off';
-
-# If no parameter is passed in, show the query screen
-if (((! isset($_GET['go'])) and ! isset($_GET['just_results'])) or
-    ($_GET['1-page'] == 'on')) {
-
-    print dump_query_screen();
-}
-
-if (isset($_GET['go'])) {
+if (isset($_GET['go']))
     print dump_report();
-}
 
 exit;
 
