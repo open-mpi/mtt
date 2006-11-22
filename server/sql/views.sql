ba47b7d1bff577f01bf5914b929d33a15ccf1d78
Ethan Mallove <Ethan.Mallove@oracle.com>
2006-11-22 23:30:59 +0000

    Added drill-down links in summary table results...
    Added drill-down links in summary table results headers,
    which entails:
     * Results and phase filtering
     * SQL views for individual phases
     * Print-detailed-report function
    
    This commit was SVN r450.

--- server/sql/views.sql
+++ server/sql/views.sql
@@ -119,3 +119,164 @@ FROM
                     mpi_get))
     ON (results.phase = 3 AND phase_id = test_run_id)
 ;
+
+DROP VIEW mpi_install_view CASCADE;
+CREATE VIEW mpi_install_view AS
+SELECT
+
+    -- submit
+    http_username,
+    local_username,
+    hostname,
+
+    bitness,
+    compiler_name,
+    compiler_version,
+    configure_arguments,
+    mpi_name,
+    mpi_version,
+    os_name,
+    os_version,
+    platform_hardware,
+    platform_name,
+    platform_type,
+
+    -- results
+    exit_status,
+    start_timestamp,
+    duration,
+    result_message,
+    result_stdout,
+    result_stderr,
+    environment,
+    client_serial,
+
+    (CASE WHEN
+      test_result = 0
+      THEN '_if' END) as fail,
+    (CASE WHEN
+      test_result = 1
+      THEN '_ip' END) as pass,
+    (CASE WHEN
+      test_result = 2
+      THEN '_is' END) as skipped,
+    (CASE WHEN
+      test_result = 3
+      THEN '_it' END) as timed_out
+FROM
+    (results NATURAL JOIN submit) JOIN 
+        (mpi_install NATURAL JOIN
+          compute_cluster NATURAL JOIN
+            compiler NATURAL JOIN
+                mpi_get)
+    ON (results.phase = 1 AND phase_id = mpi_install_id)
+;
+
+DROP VIEW test_build_view CASCADE;
+CREATE VIEW test_build_view AS
+SELECT
+
+    -- submit
+    http_username,
+    local_username,
+    hostname,
+
+    bitness,
+    compiler_name,
+    compiler_version,
+    configure_arguments,
+    mpi_name,
+    mpi_version,
+    os_name,
+    os_version,
+    platform_hardware,
+    platform_name,
+    platform_type,
+
+    -- results
+    exit_status,
+    start_timestamp,
+    duration,
+    result_message,
+    result_stdout,
+    result_stderr,
+    environment,
+    client_serial,
+
+    (CASE WHEN
+      test_result = 0
+      THEN '_bf' END) as fail,
+    (CASE WHEN
+      test_result = 1
+      THEN '_bp' END) as pass,
+    (CASE WHEN
+      test_result = 2
+      THEN '_bs' END) as skipped,
+    (CASE WHEN
+      test_result = 3
+      THEN '_bt' END) as timed_out
+FROM
+    (results NATURAL JOIN submit) JOIN 
+        (test_build NATURAL JOIN
+            (mpi_install NATURAL JOIN
+              compute_cluster NATURAL JOIN
+                compiler NATURAL JOIN
+                    mpi_get))
+    ON (results.phase = 2 AND phase_id = test_build_id)
+;
+
+
+DROP VIEW test_run_view CASCADE;
+CREATE VIEW test_run_view AS
+SELECT
+
+    -- submit
+    http_username,
+    local_username,
+    hostname,
+
+    bitness,
+    compiler_name,
+    compiler_version,
+    configure_arguments,
+    mpi_name,
+    mpi_version,
+    os_name,
+    os_version,
+    platform_hardware,
+    platform_name,
+    platform_type,
+
+    -- results
+    exit_status,
+    start_timestamp,
+    duration,
+    result_message,
+    result_stdout,
+    result_stderr,
+    environment,
+    client_serial,
+
+    (CASE WHEN
+      test_result = 0
+      THEN '_rf' END) as fail,
+    (CASE WHEN
+      test_result = 1
+      THEN '_rp' END) as pass,
+    (CASE WHEN
+      test_result = 2
+      THEN '_rs' END) as skipped,
+    (CASE WHEN
+      test_result = 3
+      THEN '_rt' END) as timed_out
+FROM
+    (results NATURAL JOIN submit)
+    JOIN test_run NATURAL JOIN
+        (test_build NATURAL JOIN
+            (mpi_install NATURAL JOIN
+              compute_cluster NATURAL JOIN
+                compiler NATURAL JOIN
+                    mpi_get))
+    ON (results.phase = 3 AND phase_id = test_run_id)
+;
+
