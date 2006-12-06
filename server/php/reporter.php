301ba5369d8c2f431730bfaba5c602e3018232a0
Ethan Mallove <Ethan.Mallove@oracle.com>
2006-12-06 21:12:31 +0000

    * Added Performance graphs (Refs #117)  * Added on-demand permalinks (Fixes #98)  * Added JpGraph [1] files to repository
    [1] [http://www.aditus.nu/jpgraph JpGraph] 
    
    This commit was SVN r469.

--- server/php/reporter.php
+++ server/php/reporter.php
@@ -1,4 +1,3 @@
-<html>
 <?php
 
 #
@@ -41,10 +40,19 @@ include_once("/l/osl/www/doc/www.open-mpi.org/dbpassword.inc");
 include_once("$topdir/reporter.inc");
 include_once("$topdir/screen.inc");
 include_once("$topdir/report.inc");
+include_once("$topdir/database.inc");
 
 # In case we're using this script from the command-line
 if ($argv)
     $_GET = getoptions($argv);
+
+# Create or redirect to a permalink
+$do_redir = $_GET['do_redir'];
+$make_redir = $_GET['make_redir'];
+if (! is_null($do_redir))
+    do_redir($do_redir);
+elseif (! is_null($make_redir))
+    make_redir($_GET);
 
 dump_query_screen();
 
@@ -53,6 +61,50 @@ if (isset($_GET['go']))
 
 exit;
 
+# Create a tiny URL permalink (using the current URL), and exit
+function make_redir($params) {
+
+    unset($params["make_redir"]);
+    unset($params["do_redir"]);
+
+    $qstring = arr2qstring($params);
+
+    $domain = $_SERVER['SERVER_NAME'];
+    $script = $_SERVER['SCRIPT_NAME'];
+    $url    = "http://$domain$script?$qstring";
+
+    # Create tiny URLs for the permalinks
+    $query = "SELECT permalink_id FROM permalinks WHERE permalink = '$url'";
+    $id = select_scalar($query);
+
+    if (is_null($id)) {
+        $query = "SELECT nextval('permalinks_permalink_id_seq')";
+        $id = select_scalar($query);
+        $insert = 
+            "INSERT INTO " .
+                "permalinks (permalink_id, permalink) " . 
+                "VALUES ('$id', '$url');";
+        do_pg_query($insert);
+    }
+
+    # Print tiny link in a tiny window
+    print "<html>" . 
+          html_head("Tiny link") .
+          "<body>" .
+          "<table><tr><td>" .
+          "<a class='black_ln' href='http://$domain$script?do_redir=$id'>Tiny link</a>" .
+          "</table>" .
+          "</body>" .
+          "</html>";
+    exit;
+}
+
+# Redirect the browser to the permalink shortcut
+function do_redir($id) {
+    $query = "SELECT permalink FROM permalinks WHERE permalink_id = '$id'";
+    $url = select_scalar($query);
+    header("Location: $url");
+    exit;
+}
+
 ?>
-</body>
-</html>
