9de992729a9fa004ae12df50ba08caa1cad86c33
Jeff Squyres <jsquyres@cisco.com>
2006-12-18 23:38:36 +0000

    Refs #155.
    Fix some old code that hasn't been used in ages.
    
    This commit was SVN r489.

--- lib/MTT/MPI/Get/SVN.pm
+++ lib/MTT/MPI/Get/SVN.pm
@@ -34,21 +34,21 @@ sub Get {
     # If we're not forcing, do we have a svn with the same URL already?
     if (!$force) {
         foreach my $mpi_get_key (keys(%{$MTT::MPI::sources})) {
-            next
-                if ($section ne $mpi_get_key);
+            next if ($section ne $mpi_get_key);
             
-        my $mpi_get = $MTT::MPI::sources->{$mpi_get_key};
-        foreach my $version_key (keys(%{$mpi_get})) {
-            my $source = $mpi_get->{$version_key};
-            Debug(">> have [$simple_section] version $version_key\n");
+            my $mpi_get = $MTT::MPI::sources->{$mpi_get_key};
+            foreach my $version_key (keys(%{$mpi_get})) {
+                my $source = $mpi_get->{$version_key};
+                Debug(">> have [$section] version $version_key\n");
 
-            if ($source->{module_name} eq "MTT::MPI::Get::SVN" &&
-                $source->{module_data}->{url} eq $url) {
-                
-                # We found it
-                
-                $previous_r = $source->{module_data}->{r};
-                last;
+                if ($source->{module_name} eq "MTT::MPI::Get::SVN" &&
+                    $source->{module_data}->{url} eq $url) {
+                    
+                    # We found it
+                    
+                    $previous_r = $source->{module_data}->{r};
+                    last;
+                }
             }
         }
     }
