66e9e5d4b9643cce5211b10ac6b5dc5d0dcae8d4
Ethan Mallove <Ethan.Mallove@oracle.com>
2007-02-28 19:28:20 +0000

    Prune ''speedy'' tables to a 7-day sliding wind...
    Prune ''speedy'' tables to a 7-day sliding window.
    
    This commit was SVN r633.

--- server/php/cron/prune_db.pl
+++ server/php/cron/prune_db.pl
@@ -1,109 +1,114 @@
 #!/usr/bin/env perl
 
 #
-# Prune the mtt database
+# Prune the MTT database
 #
 
 use Getopt::Std qw(getopts);
 
-getopts('dvhy');
+getopts('dvhy:b:n:');
 
 my $user = "mtt";
-my $db = "mtt3";
+my $db = "mtt3_1";
 
 # Window (in days) to keep data in database
 my $days_for_all = 30;
-my $days_for_trial = 7;
-
-die "\nUsage: $0 -y days" if ($opt_h);
+my $days_for_trial = 2;
 
 # Process command-line options
-if ($opt_d) { $debug = 1; }
-if ($opt_v) { $verbose = 1; }
+if ($opt_d) { $debug        = 1; }
+if ($opt_v) { $verbose      = 1; }
+if ($opt_h) { $help         = 1; }
 if ($opt_y) { $days_for_all = $opt_y; }
+if ($opt_b) { $db           = $opt_b; }
+if ($opt_n) { $now          = "TIMESTAMP '$opt_n'"; }
 
-# WHERE clause to DELETE trial results
-my $trial_clause = " AND results.trial = 't'";
+if (! $now) {
+    $now = "now()";
+}
 
-# We have to go in descending order
-# because of the FOREIGN KEY constraints
-my @phases = ('test_run', 'test_build', 'mpi_install');
+# Print help and exit
+if ($help) {
+    print "Usage: $0 
+        -b dataBase
+        -y daYs to count back from 'now'
+        -n set 'Now' timestamp to a constant
+        -d Debug (commands printed, not executed)
+        -v Verbose
+        -h Help\n";
+    exit;
+}
 
-my @cmds;
+# Define tables types, "speedy" and "archive"
+my $speedy = 'speedy_';
+my $archive = '';
 
-# Run DELETE then run VACUUM (to reclaim disk space)
-# (see postgresql.org/docs/7.4/interactive/sql-vacuum.html)
-$cmds[0] = <<EOT;
-    DELETE FROM %s WHERE
-        results.phase = %d AND
-        results.phase_id = %s.%s_id AND
-        results.start_timestamp < now() - interval '%d days'
-        %s;
-EOT
-$cmds[1] = "VACUUM VERBOSE ANALYZE %s;";
+# If CASCADE ON DELETE is set for the FOREGIN KEY
+# constraints, then we would only need to delete from MPI
+# Install (since the associated Test Build and Test Run rows
+# would get DELETEd in the CASCADE)
+my %phases = (
+    'test_run'    => 3,
+    'test_build'  => 2,
+    'mpi_install' => 1,
+);
 
-my $i = 3;
-foreach my $phase (@phases) {
-    foreach my $cmd (@cmds) {
+# Timekeeping
+my $start = time();
 
-        # Still waiting on how large a window of data to keep
+foreach my $type (($speedy, $archive)) {
 
-        # do_system("\npsql " .
-        #             "-d $db " .
-        #             "-U $user " .
-        #             "-c \"" .
-        #                 sprintf($cmd,
-        #                          $phase,
-        #                          $i,
-        #                          $phase,
-        #                          $phase,
-        #                          $days_for_all,
-        #                          '') .
-        #                  "\"");
-        do_system("\npsql " .
-                    "-d $db " .
-                    "-U $user " .
-                    "-c \"" .
-                        sprintf($cmd,
-                                 $phase,
-                                 $i,
-                                 $phase,
-                                 $phase,
-                                 $days_for_trial,
-                                 $trial_clause) . 
-                         "\"");
+    # We have to go in descending order
+    # because of the FOREIGN KEY constraints
+    foreach my $phase (sort keys %phases) {
+
+        $i = $phases{$phase};
+
+        # Only prune non-trial results for ''speedy'' TABLES
+        #
+        # TODO: FETCH A CONSTANT FOR NOW() IN TIMESTAMP COMPARE
+        if ($type eq $speedy) {
+            do_system(
+                "\n psql -d $db -U $user -c \"DELETE FROM ${type}${phase} WHERE " .
+                "\n     ${type}results.phase = $i AND " .
+                "\n     ${type}results.phase_id = ${type}${phase}.${phase}_id AND " .
+                "\n     ${type}results.start_timestamp < $now - INTERVAL '$days_for_all days' " .
+                "\n     ;\"");
+        }
+
+        # Prune trial results
+        do_system(
+            "\n psql -d $db -U $user -c \"DELETE FROM ${type}${phase} WHERE " .
+            "\n     ${type}results.phase = $i AND " .
+            "\n     ${type}results.phase_id = ${type}${phase}.${phase}_id AND " .
+            "\n     ${type}results.start_timestamp < $now - INTERVAL '$days_for_trial days' " .
+            "\n      AND ${type}results.trial = 't';\"");
+
+        # Run VACUUM after DELETE (to reclaim disk space)
+        do_system("\n psql -d $db -U $user -c \"VACUUM ANALYZE ${type}${phase};\"");
+
+        $i--;
     }
-    $i--;
+
+    # Lastly, prune results TABLE
+    if ($type eq $speedy) {
+        do_system(
+            "\n psql -d $db -U $user -c \"DELETE FROM ONLY ${type}results WHERE " .
+            "\n     start_timestamp < $now - INTERVAL '$days_for_all days' " .
+            "\n     ;\"");
+    }
+    do_system(
+        "\n psql -d $db -U $user -c \"DELETE FROM ONLY ${type}results WHERE " .
+        "\n     start_timestamp < $now - INTERVAL '$days_for_trial days' " .
+        "\n      AND ${type}results.trial = 't';\"");
+    do_system("\n psql -d $db -U $user -c \"VACUUM ANALYZE ${type}results;\"");
 }
 
-$cmds[0] = <<EOT;
-    DELETE FROM %s WHERE
-        start_timestamp < now() - interval '%d days'
-        %s;
-EOT
+# Timekeeping
+my $end = time();
+my $elapsed = $end - $start;
 
-foreach my $cmd (@cmds) {
-
-    # do_system("\npsql " .
-    #             "-d $db " .
-    #             "-U $user " .
-    #             "-c \"" .
-    #                 sprintf($cmd,
-    #                          'results',
-    #                          $days_for_all) .
-    #                  "\"");
-    do_system("\npsql " .
-                "-d $db " .
-                "-U $user " .
-                "-c \"" .
-                    sprintf($cmd,
-                             'results',
-                             $days_for_trial,
-                             $trial_clause) . 
-                     "\"");
-}
-
-print "\n";
+print "\n\nTotal elapsed time: $elapsed seconds\n";
 
 exit;
 
@@ -111,6 +116,12 @@ exit;
 
 sub do_system() {
     my ($cmd) = @_;
+    my $start = time();
+
     print "$cmd\n" if ($verbose or $debug);
     system $cmd if (! $debug);
+
+    my $end = time();
+    my $elapsed = $end - $start;
+    print "\nRunning time: $elapsed seconds\n";
 }
