a30340557311737dc8dbaca7774dd40dccf126fc
Jeff Squyres <jsquyres@cisco.com>
2007-08-30 14:30:29 +0000

    Put in a redirect from reporter.php to .
    This commit was SVN r961.

---
+++ server/php/reporter.php
@@ -0,0 +1,15 @@
+<?php
+
+#
+# Copyright (c) 2006 Sun Microsystems, Inc.
+#                         All rights reserved.
+# $COPYRIGHT$
+#
+# Additional copyrights may follow
+#
+# $HEADER$
+#
+
+header("Location: .");
+
+?>
