138e85bdb9cda4fe6d4f3849225ac566f042cc09
Josh Hursey <jjhursey@open-mpi.org>
2008-12-01 16:05:25 +0000

    Add lines for 2009 tables
    This commit was SVN r1257.

--- server/sql/support/create-partitions.sh
+++ server/sql/support/create-partitions.sh
@@ -8,16 +8,19 @@
 ./create-partitions-mpi-install.pl 2006 12 >> 2006-mpi-install.sql
 ./create-partitions-mpi-install.pl 2007 XX >  2007-mpi-install.sql
 ./create-partitions-mpi-install.pl 2008 XX >  2008-mpi-install.sql
+./create-partitions-mpi-install.pl 2009 XX >  2009-mpi-install.sql
 
 ./create-partitions-test-build.pl 2006 11 >  2006-test-build.sql
 ./create-partitions-test-build.pl 2006 12 >> 2006-test-build.sql
 ./create-partitions-test-build.pl 2007 XX >  2007-test-build.sql
 ./create-partitions-test-build.pl 2008 XX >  2008-test-build.sql
+./create-partitions-test-build.pl 2009 XX >  2009-test-build.sql
 
 ./create-partitions-test-run.pl 2006 11 >  2006-test-run.sql
 ./create-partitions-test-run.pl 2006 12 >> 2006-test-run.sql
 ./create-partitions-test-run.pl 2007 XX >  2007-test-run.sql
 ./create-partitions-test-run.pl 2008 XX >  2008-test-run.sql
+./create-partitions-test-run.pl 2009 XX >  2009-test-run.sql
 
 #
 # Create the indexes for these partitions
@@ -26,3 +29,4 @@
 ./create-partition-indexes.pl 2006 12 >> 2006-indexes.sql
 ./create-partition-indexes.pl 2007 XX >  2007-indexes.sql
 ./create-partition-indexes.pl 2008 XX >  2008-indexes.sql
+./create-partition-indexes.pl 2009 XX >  2009-indexes.sql
