166f609487afc2205e9522dbd0717169a9d3dd6e
Jeff Squyres <jsquyres@cisco.com>
2009-07-06 14:19:24 +0000

    Also eval the argv so that you can put interest...
    Also eval the argv so that you can put interesting (i.e., evaluated)
    stuff in there
    
    This commit was SVN r1302.

--- lib/MTT/Test/RunEngine.pm
+++ lib/MTT/Test/RunEngine.pm
@@ -191,7 +191,7 @@ sub RunEngine {
         $MTT::Test::Run::test_executable_abspath = $test_exe_abs;
         $MTT::Test::Run::test_executable_basename = $test_exe_basename;
 
-        $MTT::Test::Run::test_argv = $run->{argv};
+        $MTT::Test::Run::test_argv = MTT::Values::EvaluateString($run->{argv}, $ini, $test_run_full_name);
         my $all_np = MTT::Values::EvaluateString($run->{np}, $ini, $test_run_full_name);
 
         my $save_run_mpi_details = $MTT::Test::Run::mpi_details;
