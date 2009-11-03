56a9f71b3cf518c8adf7352d9a5de184a9241e6b
Mike Dubman <miked@mellanox.com>
2009-11-03 09:13:00 +0000

    hpcc/hpl - more params of HPL.dat can be provid...
    hpcc/hpl - more params of HPL.dat can be provided from cmd line
    
    This commit was SVN r1328.

--- client/custom_launchers/hpcc/bcreate_conf.pl
+++ client/custom_launchers/hpcc/bcreate_conf.pl
@@ -12,6 +12,95 @@ use strict;
 use Math::Complex;
 use Getopt::Long;
 
+my $hosts;
+my $dat_file = "/tmp/HPL.dat";
+my $opt_long;
+
+my $opt_hpcc;
+my $opt_nb=128;
+my $opt_ns;
+my $opt_np;
+my $opt_p;
+my $opt_q;
+my $opt_pfacts = "0 1 2";
+my $opt_rfacts = "0 1 2";
+my $opt_nbmins = "2 4";
+my $opt_ndiv   = "2";
+my $opt_help;
+my $opt_bcast = "0";
+my $opt_depth = "0";
+
+GetOptions( 
+	'l' => \$opt_long, 
+	'h|hosts=s' => \$hosts,
+	'target|t=s' =>\$dat_file, 
+	'hpcc' => \$opt_hpcc,
+	'nb=i', => \$opt_nb,
+	'ns=i', => \$opt_ns,
+	'np=i', => \$opt_np,
+	'p=i', => \$opt_p,
+	'q=i', => \$opt_q,
+	'pfacts=s' => \$opt_pfacts,
+	'rfacts=s' => \$opt_rfacts,
+	'bcast=s' => \$opt_bcast,
+	'depth=s' => \$opt_depth,
+	'nbmins=s' => \$opt_nbmins,
+	'ndiv=s' => \$opt_ndiv,
+	'help', => \$opt_help,
+) or die "Incorrect usage!\n";
+
+usage() if $opt_help;
+
+my $np=0;
+my $ns;
+
+if ( $opt_ns and $opt_np ) {
+	$np = $opt_np;
+	$ns = $opt_ns;
+
+} elsif ( defined $hosts ) {
+
+	my @arr_hosts=split(/\,/,$hosts);
+	my (@cpuinfo,@meminfo);
+
+	Collect_Hosts_Info(\@cpuinfo,\@meminfo,@arr_hosts);
+	print ("meminfo=@meminfo\n");
+	print ("cpuinfo=@cpuinfo\n");
+	foreach my $cpu (@cpuinfo) {
+		$np+=$cpu;
+	}
+	my $ram_per_core=($meminfo[0]/($cpuinfo[0]*1000000));
+	if($ram_per_core >= 0.9) {
+		$ram_per_core=1;
+	}
+
+	$ns=int(sqrt(0.8*$ram_per_core*1024*1024*1024*$np/8));
+	print ("ram_per_core=$ram_per_core\n");
+} else {
+	usage();
+}
+
+
+my (@p,@q);
+if ( not defined $opt_p and not defined $opt_q ) {
+	if ( $opt_long ) {
+		Calc_NP(\@p,\@q,$np);
+	} else {
+		Calc_Np_Short(\@p,\@q,$np);
+	}
+} else {
+	push @p, $opt_p;
+	push @q, $opt_q;
+}
+
+print ("NP=$np, NS=$ns, P=@p, Q=@q\n");
+
+Generate_HPL_DAT($ns,$opt_nb,\@p,\@q,$dat_file,$opt_hpcc);
+
+print ("dat file : $dat_file\n");
+
+##############################################################
+
 
 sub Collect_Hosts_Info
 {
@@ -61,19 +150,34 @@ sub Calc_NP
     }
 }
 
+sub count_items
+{
+	my ($str) = @_;
+	my @items = split(" ",$str);
+	return $#items + 1;
+}
+
 sub Generate_HPL_DAT
 {
 	my ($Ns,$Nb, $p,$q,$hpl,$hpcc)=@_;
 
+	my $nPfacts = count_items($opt_pfacts);
+	my $nRfacts = count_items($opt_rfacts);
+	my $nBcast  = count_items($opt_bcast);
+	my $nDepth  = count_items($opt_depth);
+	my $nNbmins = count_items($opt_nbmins);
+	my $nNb     = count_items($Nb);
+	my $nNs     = count_items($Ns);
+	my $nNdiv   = count_items($opt_ndiv);
 
 	open HPL,">$hpl" or die "$!";
 	print HPL  "HPLinpack benchmark input file\n" ;
 	print HPL  "Innovative Computing Laboratory, University of Tennessee\n";
 	print HPL  "HPL.out      output file name (if any)\n";
 	print HPL  "6            device out (6=stdout,7=stderr,file)\n";
-	print HPL  "1            # of problems sizes (N)\n";
+	print HPL  "$nNs            # of problems sizes (N)\n";
 	print HPL "$Ns  Ns\n";
-	print HPL  "1            # of NBs\n";
+	print HPL  "$nNb            # of NBs\n";
 	print HPL "$Nb      NBs\n";
 	print HPL "0            PMAP process mapping (0=Row-,1=Column-major)\n";
 	my $size=@$p;
@@ -81,24 +185,25 @@ sub Generate_HPL_DAT
 	print HPL "@$p        Ps\n";
 	print HPL "@$q        Qs\n";
 	print HPL "16.0         threshold\n";
-	print HPL  "3            # of panel fact\n";
-	print HPL "0 1 2        PFACTs (0=left, 1=Crout, 2=Right)\n";
-	print HPL "2            # of recursive stopping criterium\n";
-	print HPL "2 4          NBMINs (>= 1)\n";
-	print HPL "1            # of panels in recursion\n";
-	print HPL "2            NDIVs\n";
-	print HPL "3            # of recursive panel fact.\n";
-	print HPL "0 1 2        RFACTs (0=left, 1=Crout, 2=Right)\n";
-	print HPL "1            # of broadcast\n";
-	print HPL "0            BCASTs (0=1rg,1=1rM,2=2rg,3=2rM,4=Lng,5=LnM)\n";
-	print HPL "1            # of lookahead depth\n";
-	print HPL "0            DEPTHs (>=0)\n";
+	print HPL "$nPfacts            # of panel fact\n";
+	print HPL "$opt_pfacts        PFACTs (0=left, 1=Crout, 2=Right)\n";
+	print HPL "$nNbmins            # of recursive stopping criterium\n";
+	print HPL "$opt_nbmins          NBMINs (>= 1)\n";
+	print HPL "$nNdiv            # of panels in recursion\n";
+	print HPL "$opt_ndiv            NDIVs\n";
+	print HPL "$nRfacts            # of recursive panel fact.\n";
+	print HPL "$opt_rfacts        RFACTs (0=left, 1=Crout, 2=Right)\n";
+	print HPL "$nBcast            # of broadcast\n";
+	print HPL "$opt_bcast            BCASTs (0=1rg,1=1rM,2=2rg,3=2rM,4=Lng,5=LnM)\n";
+	print HPL "$nDepth            # of lookahead depth\n";
+	print HPL "$opt_depth            DEPTHs (>=0)\n";
 	print HPL "2            SWAP (0=bin-exch,1=long,2=mix)\n";
 	print HPL "64           swapping threshold\n";
 	print HPL "0            L1 in (0=transposed,1=no-transposed) form\n";
 	print HPL "0            U  in (0=transposed,1=no-transposed) form\n";
 	print HPL "1            Equilibration (0=no,1=yes)\n" ;
-	print HPL "8            memory alignment in double (> 0)\n";
+    print HPL "8            memory alignment in double (> 0)\n";
+
 	if ($hpcc){
 	    print HPL "##### This line (no. 32) is ignored (it serves as a separator). ######\n";
 	    print HPL "0                               Number of additional problem sizes for PTRANS\n";
@@ -113,7 +218,8 @@ sub Generate_HPL_DAT
 sub usage{
 	my $myname = `basename $0`;
 	chomp($myname);
-    my $help = "Usage: $myname [-p <int>] [-q <int] [-np <int>] [-ns <int>] [-nb <int>] [-l] <-h host1,host2,host3> <-t /path/to/HPL.dat>
+
+    my $help = "Usage: $myname [-p <int>] [-q <int] [-np <int>] [-ns <int>] [-nb <int>] [-pfacts <str>] [-rfacts <str>] [-depth <str>] [-nbmins <str>] [-ndiv <str>] [-l] <-h host1,host2,host3> <-t /path/to/HPL.dat>
 
 		where
 			-l - generate HPL.dat with big sized problem definiton
@@ -124,75 +230,3 @@ sub usage{
 }
 
 ##############################################################
-my $hosts;
-my $dat_file = "/tmp/HPL.dat";
-my $opt_long;
-
-my $opt_hpcc;
-my $opt_nb=128;
-my $opt_ns;
-my $opt_np;
-my $opt_p;
-my $opt_q;
-my $opt_help;
-
-GetOptions( 
-	'l' => \$opt_long, 
-	'h|hosts=s' => \$hosts,
-	'target|t=s' =>\$dat_file, 
-	'hpcc' => \$opt_hpcc,
-	'nb=i', => \$opt_nb,
-	'ns=i', => \$opt_ns,
-	'np=i', => \$opt_np,
-	'p=i', => \$opt_p,
-	'q=i', => \$opt_q,
-	'help', => \$opt_help,
-) or die "Incorrect usage!\n";
-
-usage() if $opt_help;
-
-my $np=0;
-my $ns;
-
-if ( $opt_ns and $opt_np ) {
-	$np = $opt_np;
-	$ns = $opt_ns;
-
-} elsif ( defined $hosts ) {
-
-	my @arr_hosts=split(/\,/,$hosts);
-	my (@cpuinfo,@meminfo);
-
-	Collect_Hosts_Info(\@cpuinfo,\@meminfo,@arr_hosts);
-	print ("meminfo=@meminfo\n");
-	print ("cpuinfo=@cpuinfo\n");
-	foreach my $cpu (@cpuinfo) {
-		$np+=$cpu;
-	}
-	my $ram_per_core=($meminfo[0]/($cpuinfo[0]*1000000));
-	if($ram_per_core >= 0.9) {
-		$ram_per_core=1;
-	}
-
-	$ns=int(sqrt(0.8*$ram_per_core*1024*1024*1024*$np/8));
-	print ("ram_per_core=$ram_per_core\n");
-}
-
-
-my (@p,@q);
-if ( not defined $opt_p and not defined $opt_q ) {
-	if ( $opt_long ) {
-		Calc_NP(\@p,\@q,$np);
-	} else {
-		Calc_Np_Short(\@p,\@q,$np);
-	}
-} else {
-	push @p, $opt_p;
-	push @q, $opt_q;
-}
-
-print ("NP=$np, NS=$ns, P=@p, Q=@q\n");
-
-Generate_HPL_DAT($ns,$opt_nb,\@p,\@q,$dat_file,$opt_hpcc);
-
-print ("dat file : $dat_file\n");
