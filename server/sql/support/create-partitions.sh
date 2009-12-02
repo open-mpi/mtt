a0280b23431c622e1d51142737060b8807d13045
Josh Hursey <jjhursey@open-mpi.org>
2009-12-02 15:43:03 +0000

    Add the 2010 tables to the MTT database.
    Update the 'create-partitions.sh' archive script to reflect the new additions.
    
    Add a README so that others can add next years tables in case I'm not around.
    
    This commit was SVN r1340.

--- server/sql/support/create-partitions.sh
+++ server/sql/support/create-partitions.sh
@@ -1,5 +1,5 @@
 #
-# This script just generates the partiion table files for 2006 - 2008
+# This script just generates the partiion table files for 2006 - 2010
 #
 # It should be used as an example for how to run the generation scripts
 # as we don't want to replace the existing partition tables going forward.
@@ -9,18 +9,21 @@
 ./create-partitions-mpi-install.pl 2007 XX >  2007-mpi-install.sql
 ./create-partitions-mpi-install.pl 2008 XX >  2008-mpi-install.sql
 ./create-partitions-mpi-install.pl 2009 XX >  2009-mpi-install.sql
+./create-partitions-mpi-install.pl 2010 XX >  2010-mpi-install.sql
 
 ./create-partitions-test-build.pl 2006 11 >  2006-test-build.sql
 ./create-partitions-test-build.pl 2006 12 >> 2006-test-build.sql
 ./create-partitions-test-build.pl 2007 XX >  2007-test-build.sql
 ./create-partitions-test-build.pl 2008 XX >  2008-test-build.sql
 ./create-partitions-test-build.pl 2009 XX >  2009-test-build.sql
+./create-partitions-test-build.pl 2010 XX >  2010-test-build.sql
 
 ./create-partitions-test-run.pl 2006 11 >  2006-test-run.sql
 ./create-partitions-test-run.pl 2006 12 >> 2006-test-run.sql
 ./create-partitions-test-run.pl 2007 XX >  2007-test-run.sql
 ./create-partitions-test-run.pl 2008 XX >  2008-test-run.sql
 ./create-partitions-test-run.pl 2009 XX >  2009-test-run.sql
+./create-partitions-test-run.pl 2010 XX >  2010-test-run.sql
 
 #
 # Create the indexes for these partitions
@@ -30,3 +33,4 @@
 ./create-partition-indexes.pl 2007 XX >  2007-indexes.sql
 ./create-partition-indexes.pl 2008 XX >  2008-indexes.sql
 ./create-partition-indexes.pl 2009 XX >  2009-indexes.sql
+./create-partition-indexes.pl 2010 XX >  2010-indexes.sql
