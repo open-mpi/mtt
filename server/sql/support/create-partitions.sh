026906ef95613dd2046bbca2ba956aa516b87ca7
Josh Hursey <jjhursey@open-mpi.org>
2010-12-29 16:13:02 +0000

    add 2011 lines
    This commit was SVN r1379.

--- server/sql/support/create-partitions.sh
+++ server/sql/support/create-partitions.sh
@@ -10,6 +10,7 @@
 ./create-partitions-mpi-install.pl 2008 XX >  2008-mpi-install.sql
 ./create-partitions-mpi-install.pl 2009 XX >  2009-mpi-install.sql
 ./create-partitions-mpi-install.pl 2010 XX >  2010-mpi-install.sql
+./create-partitions-mpi-install.pl 2011 XX >  2011-mpi-install.sql
 
 ./create-partitions-test-build.pl 2006 11 >  2006-test-build.sql
 ./create-partitions-test-build.pl 2006 12 >> 2006-test-build.sql
@@ -17,6 +18,7 @@
 ./create-partitions-test-build.pl 2008 XX >  2008-test-build.sql
 ./create-partitions-test-build.pl 2009 XX >  2009-test-build.sql
 ./create-partitions-test-build.pl 2010 XX >  2010-test-build.sql
+./create-partitions-test-build.pl 2011 XX >  2011-test-build.sql
 
 ./create-partitions-test-run.pl 2006 11 >  2006-test-run.sql
 ./create-partitions-test-run.pl 2006 12 >> 2006-test-run.sql
@@ -24,6 +26,7 @@
 ./create-partitions-test-run.pl 2008 XX >  2008-test-run.sql
 ./create-partitions-test-run.pl 2009 XX >  2009-test-run.sql
 ./create-partitions-test-run.pl 2010 XX >  2010-test-run.sql
+./create-partitions-test-run.pl 2011 XX >  2011-test-run.sql
 
 #
 # Create the indexes for these partitions
@@ -34,3 +37,4 @@
 ./create-partition-indexes.pl 2008 XX >  2008-indexes.sql
 ./create-partition-indexes.pl 2009 XX >  2009-indexes.sql
 ./create-partition-indexes.pl 2010 XX >  2010-indexes.sql
+./create-partition-indexes.pl 2011 XX >  2011-indexes.sql
