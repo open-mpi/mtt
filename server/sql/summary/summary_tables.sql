9bc7380d76058d2ed425c95d2eaecc79a657e6cf
Josh Hursey <jjhursey@open-mpi.org>
2011-08-24 15:52:46 +0000

    add a quick lookup date range table for summary...
    add a quick lookup date range table for summary lookups
    
    This commit was SVN r1412.

--- server/sql/summary/summary_tables.sql
+++ server/sql/summary/summary_tables.sql
@@ -3,6 +3,38 @@ DROP TABLE summary_test_build;
 DROP TABLE summary_test_run;
 DROP TABLE summary_base;
 
+DROP TABLE summary_range;
+
+--
+-- A table to quickly lookup the earliest date for which there is summary data.
+--
+CREATE TABLE summary_range (
+    range_id          serial,
+    start_timestamp   timestamp without time zone NOT NULL DEFAULT now() - interval '24 hours',
+    end_timestamp     timestamp without time zone NOT NULL DEFAULT now() - interval '24 hours',
+    time_interval     interval
+);
+
+-- INSERT INTO summary_range
+--     (SELECT 1, min_start, min_end, (min_end-min_start) as interval
+--      FROM (
+--         SELECT
+--             MIN(start_timestamp) as min_start,
+--             MIN(end_timestamp) as min_end
+--         FROM summary_mpi_install
+--         ) as foo
+--     );
+
+-- UPDATE summary_range
+--     SET
+--         start_timestamp = (SELECT MIN(start_timestamp) FROM summary_mpi_install),
+--         end_timestamp = (SELECT MIN(end_timestamp) FROM summary_mpi_install),
+--         time_interval = (SELECT (MIN(end_timestamp) - MIN(start_timestamp)) FROM summary_mpi_install)
+--     WHERE range_id = 1;
+
+--
+-- Base table for summary data
+--
 CREATE TABLE summary_base (
     -- select date_trunc('hour', now()) --
     start_timestamp   timestamp without time zone NOT NULL DEFAULT now() - interval '24 hours',
@@ -44,12 +76,18 @@ CREATE TABLE summary_base (
     fail smallint NOT NULL DEFAULT '0'
 );
 
+--
+-- Summary table for mpi_install
+--
 CREATE TABLE summary_mpi_install (
     summary_mpi_install_id      serial,
     -- ********** --
     PRIMARY KEY (summary_mpi_install_id)
 ) INHERITS(summary_base);
 
+--
+-- Summary table for test_build
+--
 CREATE TABLE summary_test_build (
     summary_test_build_id      serial,
 
@@ -62,6 +100,9 @@ CREATE TABLE summary_test_build (
     PRIMARY KEY (summary_test_build_id)
 ) INHERITS(summary_base);
 
+--
+-- Summary table for test_run
+--
 CREATE TABLE summary_test_run (
     summary_test_run_id      serial,
 
