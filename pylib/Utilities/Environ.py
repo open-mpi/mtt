cabd8b9d56a320abd12c98c1e26037d351944014
Noah van Dresser <daniel.n.van.dresser@intel.com>
2016-07-08 10:42:23 -0700

    Added Environ plugin which enables setting environment variables. Modified configparse optionxform str to make option names case sensitive. Bug fix for ModuleCmd to use io.StringIO.seek() instead of StringIO.StringIO.len().
    Signed-off-by: Noah van Dresser <daniel.n.van.dresser@intel.com>

---
+++ pylib/Utilities/Environ.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2015-2016 Intel, Inc. All rights reserved.
+# $COPYRIGHT$
+#
+# Additional copyrights may follow
+#
+# $HEADER$
+#
+
+import shutil
+import os
+from BaseMTTUtility import *
+
+class Environ(BaseMTTUtility):
+    def __init__(self):
+        BaseMTTUtility.__init__(self)
+        self.options = {}
+
+    def print_name(self):
+        return "Environ"
+
+    def print_options(self, testDef, prefix):
+        lines = testDef.printOptions(self.options)
+        for line in lines:
+            print prefix + line
+        return
+
+    def execute(self, log, keyvals, testDef):
+        testDef.logger.verbose_print("Environ Execute")
+
+        # Set any provided key values
+        kvkeys = keyvals.keys()
+        for kvkey in kvkeys:
+            os.environ[kvkey] = keyvals[kvkey]
+        log['status'] = 0
+        return
