37ca8dad0d48ee1c8b6bac10363beced40f7d63d
Deb Rezanka <drezanka@newmexicoconsortium.org>
2016-08-18 11:00:40 -0600

    futurized Environ.py,
    working on combinatorial executor and adding pytests to the system
    
    Signed-off-by: Deb Rezanka <drezanka@newmexicoconsortium.org>

---
+++ tests/pytestDir/conftest.py
@@ -0,0 +1,20 @@
+# place pytest fixtures here
+import pytest
+
+'''
+@pytest.fixture(scope='session')
+def foo(request):
+    print("Session setup")
+    def fin():
+        print("session finalizer")
+    request.addfinalizer(fin)
+
+@pytest.fixture(scope='function')
+def bar(request):
+    print('function setup')
+    def fin():
+        print('function finalizer')
+    request.addfinalizer(fin)
+'''
+
+#@pytest.fixture
