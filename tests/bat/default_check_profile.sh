4a594e2bad21edb0a4d843a88c5dc44053c08a2f
Noah van Dresser <daniel.n.van.dresser@intel.com>
2016-09-06 17:22:39 -0700

    Sample BAT test for Profile/DefaultProfile and README.bat.txt with list of needed BAT tests.
    Signed-off-by: Noah van Dresser <daniel.n.van.dresser@intel.com>

---
+++ tests/bat/default_check_profile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+
+### INPUTS via environment variables ###
+#  KERNEL_RELEASE
+#  LOGFILE
+
+kernel_release=${KERNEL_RELEASE:? KERNEL_RELEASE not set}
+logfile=${LOGFILE:? LOGFILE not set}
+
+# Check that the logfile has correct version string
+# Remove the line in the logfile that was used for passing in KERNEL_RELEASE environment variable
+# The logfile will live at the top of the <scratchdir>/<testname> directory structure
+cat ../../$logfile | grep -v KERNEL_RELEASE | grep $kernel_release
+exit $?
