4af136a030a2f4c2a07aec33255692941d23ced4
Howard Pritchard <howardp@lanl.gov>
2018-01-08 16:16:45 -0700

    pyclient: squash some Watchdog problems set timeout default to 6 minutes.
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- pylib/Utilities/Watchdog.py
+++ pylib/Utilities/Watchdog.py
@@ -3,6 +3,8 @@ from builtins import str
 #!/usr/bin/env python
 #
 # Copyright (c) 2015-2016 Intel, Inc. All rights reserved.
+# Copyright (c) 2018      Los Alamos National Security, LLC. 
+#                         All rights reserved.
 # $COPYRIGHT$
 #
 # Additional copyrights may follow
@@ -21,9 +23,10 @@ from BaseMTTUtility import *
 # @param  timeout  Time in seconds before generating exception
 # @}
 class Watchdog(BaseMTTUtility):
-    def __init__(self, timeout):
+    def __init__(self, timeout=360):
         BaseMTTUtility.__init__(self)
         self.timeout = timeout
+        self.options = {}
         self.options['time'] = (None, "Time in seconds before generating exception")
         self.timer = Timer(self.timeout, self.defaultHandler)
 
