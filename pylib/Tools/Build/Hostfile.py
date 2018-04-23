9204bea1aaf635670eb56a5c324306cc9f6dd807
Richard Barella <richard.t.barella@intel.com>
2018-04-23 14:10:26 -0700

    Hostfile plugin can now specify fraction of nodes
    Signed-off-by: Richard Barella <richard.t.barella@intel.com>

--- pylib/Tools/Build/Hostfile.py
+++ pylib/Tools/Build/Hostfile.py
@@ -13,6 +13,8 @@ import os
 import re
 from BuildMTTTool import *
 import hostlist # python-hostlist package
+import shlex
+import math
 
 ## @addtogroup Tools
 # @{
@@ -29,7 +31,12 @@ class Hostfile(BuildMTTTool):
         self.options = {}
         self.options['parent'] = (None, "Section that precedes this one in the dependency tree")
         self.options['nodelist'] = (None, "List of nodes to create hostfile from")
+        self.options['nodeuse_ratio'] = (None, "Fraction of nodes to use")
         self.options['hostfile'] = (None, "Name of hostfile to generate")
+        self.options['nodestatus_cmd'] = ("sinfo -N -h", "Command to print status of nodes")
+        self.options['nodename_column'] = ("0", "Column index from nodestatus_cmd where nodename resides")
+        self.options['nodestatus_column'] = ("3", "Column index from nodestatus_cmd where status of node resides")
+        self.options['idle_status_string'] = ("idle", "String to check for when checking node status")
         return
 
     def activate(self):
@@ -60,13 +67,14 @@ class Hostfile(BuildMTTTool):
         cmds = {}
         testDef.parseOptions(log, self.options, keyvals, cmds)
 
-        # If they didn't give us a hostfile and nodelist, then error out
+        # If they didn't give us a hostfile and nodelist (or nodeuse_ratio), then error out
+
         try:
             wrong_input_stderr = ""
             if cmds['hostfile'] is None:
                 wrong_input_stderr += "No hostfile specified. "
-            if cmds['nodelist'] is None:
-                wrong_input_stderr += "No nodelist specified. "
+            if cmds['nodelist'] is None and cmds['nodeuse_ratio'] is None:
+                wrong_input_stderr += "No nodelist specified, or nodeuse_ratio specified. "
             if wrong_input_stderr:
                 log['status'] = 1
                 log['stderr'] = wrong_input_stderr
@@ -146,13 +154,69 @@ class Hostfile(BuildMTTTool):
         ################################
         # Execute Plugin
         ################################
-        
-        # Create hostlist from nodelist
-        try:
-            hosts = hostlist.expand_hostlist(cmds['nodelist'])
-        except hostlist.BadHostlist:
+        status, stdout, stderr, time = testDef.execmd.execute(cmds, shlex.split(cmds['nodestatus_cmd']), testDef)
+        if status != 0:
             log['status'] = 1
-            log['stderr'] = "Bad nodelist format: %s" % cmds['nodelist']
+            log['stderr'] = "Command %s failed: %s" % (cmds['nodestatus_cmd'], " ".join(stderr))
+            os.chdir(cwd)
+            return
+        stdout_split = [l.split() for l in stdout]
+        try:
+            nodename_column = int(cmds['nodename_column'])
+        except ValueError:
+            log['status'] = 1
+            log['stderr'] = "Invalid nodename_column %s -- must be an integer" % cmds['nodename_column']
+            os.chdir(cwd)
+            return
+        try:
+            nodestatus_column = int(cmds['nodestatus_column'])
+        except ValueError:
+            log['status'] = 1
+            log['stderr'] = "Invalid nodestatus_column %s -- must be an integer" % cmds['nodestatus_column']
+        try:
+            node_status = {l[nodename_column]: l[nodestatus_column] for l in stdout_split}
+        except IndexError:
+            log['status'] = 1
+            log['stderr'] = "" 
+            if nodename_column < 0 or nodename_column >= len(stdout_split[0]):
+                log['stderr'] += "nodename_column is out of bounds: %s  " % str(nodename_column)
+            if nodestatus_column < 0 or nodestatus_column >= len(stdout_split[0]):
+                log['stderr'] += "nodestatus_column is out of bounds: %s  " % str(nodestatus_column)
+            os.chdir(cwd)
+            return
+
+        if cmds['nodelist'] is not None:
+            # Create hostlist from nodelist
+            try:
+                hosts = hostlist.expand_hostlist(cmds['nodelist'])
+            except hostlist.BadHostlist:
+                log['status'] = 1
+                log['stderr'] = "Bad nodelist format: %s" % cmds['nodelist']
+                os.chdir(cwd)
+                return
+        else:
+            # use all hosts
+            hosts = [l[nodename_column] for l in stdout_split]
+
+        # Use a ratio of the nodes
+        if cmds['nodeuse_ratio'] is not None:
+            try:
+                ratio = float(cmds['nodeuse_ratio'])
+            except:
+                try:
+                    ratio = float(cmds['nodeuse_ratio'].split("/")[0])/float(cmds['nodeuse_ratio'].split("/")[1])
+                except:
+                    log['status'] = 1
+                    log['stderr'] = "Bad nodeuse_ratio format: %s" % cmds['nodeuse_ratio']
+                    os.chdir(cwd)
+                    return
+            idle_hosts = [h for h in hosts if node_status[h] == cmds['idle_status_string']]
+            nonidle_hosts = [h for h in hosts if node_status[h] != cmds['idle_status_string']]
+            num_nodes = int(math.ceil(float(len(hosts))*ratio))
+            if len(idle_hosts) <= num_nodes:
+                hosts = idle_hosts[:num_nodes]
+            else:
+                hosts = idle_hosts + nonidle_hosts[:num_nodes - len(idle_hosts)]
 
         # Create hostfile from hostlist
         try:
@@ -162,6 +226,8 @@ class Hostfile(BuildMTTTool):
         except IOError:
             log['status'] = 1
             log['stderr'] = "File exception when writing hostfile %s" % cmds['hostfile']
+            os.chdir(cwd)
+            return
 
         log['status'] = 0
         log['stdout'] = "Creation of hostfile %s from nodelist %s success" % (cmds['hostfile'],cmds['nodelist'])
